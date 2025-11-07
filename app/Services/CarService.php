1e6b91887b2ef7d97e04b81908c6b7e5cbb3d4cc
aleksandra <sdartanool2003@gmail.com>
2025-11-08 00:42:56 +0300

    Add CarServices

---
+++ app/Services/CarService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services;
+
+use App\DTOs\CarDTO;
+use App\Models\Car;
+use Carbon\Carbon;
+use Ramsey\Collection\Collection;
+
+class CarService
+{
+    public function getAvailableCars(CarDTO $dto)
+    {
+        $user = \App\Models\User::where('email', $dto->email)->first();
+        if (!$user) {
+            return response()->json(['message' => 'Пользователь с таким email не найден'], 404);
+        }
+
+        $start = Carbon::parse($dto->startTime);
+        $end = Carbon::parse($dto->endTime);
+
+        $allowedCategories = $user->position
+            ->comfortCategories()
+            ->pluck('comfort_categories.id')
+            ->toArray();
+
+        $query = Car::query()
+            ->whereHas('carModel', function ($q) use ($allowedCategories) {
+                $q->whereIn('comfort_category_id', $allowedCategories);
+            });
+
+        if (!empty($dto->modelId)) {
+            $query->where('car_model_id', $dto->modelId);
+        }
+
+        if (!empty($dto->categoryId)) {
+            $query->whereHas('model', function ($q) use ($dto) {
+                $q->where('comfort_category_id', $dto->categoryId);
+            });
+        }
+
+        $query->whereDoesntHave('bookings', function ($q) use ($start, $end) {
+            $q->where(function ($q2) use ($start, $end) {
+                $q2->whereBetween('starts_at', [$start, $end])
+                    ->orWhereBetween('ends_at', [$start, $end])
+                    ->orWhere(function ($q3) use ($start, $end) {
+                        $q3->where('starts_at', '<', $start)
+                            ->where('ends_at', '>', $end);
+                    });
+            });
+        });
+
+        $cars = $query
+            ->with(['carModel.comfortCategory', 'driver'])
+            ->get();
+
+        return $cars;
+    }
+}
